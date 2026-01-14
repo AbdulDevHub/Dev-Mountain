2f778f558eae5e266505a821b03003262c746551
Abdul Khan <ahkn63@gmail.com>
2026-01-14 17:00:30 -0500

    ✨ feat(resources): add resources page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,123 +1,152 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer"
+import type { Config } from "@docusaurus/types"
+import type * as Preset from "@docusaurus/preset-classic"
 
 const config: Config = {
-  title: 'Dev Dino Mountain',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: "Dev Dino Mountain",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'abdulDevHub', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "abdulDevHub", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/AbdulDevHub/Docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/AbdulDevHub/Docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/AbdulDevHub/Docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/AbdulDevHub/Docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
 
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "resources",
+        path: "resources",
+        routeBasePath: "resources",
+        sidebarPath: "./sidebars-resources.ts",
+        editUrl: "https://github.com/AbdulDevHub/Docusaurus/tree/main/",
+      },
+    ],
+  ],
+
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'Dev Dino Mountain',
+      title: "Dev Dino Mountain",
       logo: {
-        alt: 'Dev Dino Mountain Logo',
-        src: 'img/logo.svg',
+        alt: "Dev Dino Mountain Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {to: 'about', label: 'About', position: 'left'},
         {
-          href: 'https://github.com/AbdulDevHub/Docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          type: "docSidebar",
+          sidebarId: "resourcesSidebar",
+          docsPluginId: "resources",
+          position: "left",
+          label: "Resources",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+        { to: "about", label: "About", position: "left" },
+        {
+          href: "https://github.com/AbdulDevHub/Dev-Mountain",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+            {
+              label: "Resources",
+              to: "/resources/learning-platforms",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: "Blog",
+              to: "/blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/AbdulDevHub/Docusaurus',
+              label: "GitHub",
+              href: "https://github.com/AbdulDevHub/Docusaurus",
             },
           ],
         },
@@ -129,6 +158,6 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
-};
+}
 
-export default config;
+export default config
---
+++ sidebars-resources.ts
@@ -0,0 +1,32 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs"
+
+const sidebars: SidebarsConfig = {
+  resourcesSidebar: [
+    {
+      type: "doc",
+      id: "learning-platforms",
+    },
+    {
+      type: "doc",
+      id: "ui-libraries-and-animation",
+    },
+    {
+      type: "doc",
+      id: "ui-motion-and-inspiration",
+    },
+    {
+      type: "doc",
+      id: "frontend-snippets",
+    },
+    {
+      type: "doc",
+      id: "asset-filenames",
+    },
+    {
+      type: "doc",
+      id: "commit-lint-guide",
+    },
+  ],
+}
+
+export default sidebars
