898eea2e648f80d6f0826d35f66cebd8f66e75c4
Abdul Khan <ahkn63@gmail.com>
2026-07-08 18:40:33 -0400

    fix: broken link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/resources/index">
+            to="/resources">
             Explore Resources →
           </Link>
         </div>
